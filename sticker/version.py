0ab94830c17ef1793124bf2db61744405f84085e
Ren Tatsumoto <tatsu@autistici.org>
2022-05-28 15:31:36 +0300

    update ajatt

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '77fd5481'
+version = '0.1.0+dev.77fd5481'
+linkified_version = '0.1.0+dev.[77fd5481](https://github.com/maunium/stickerpicker/commit/77fd5481a3cb31341ecdbafec607bae0a274e526)'
